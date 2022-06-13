463a720c844f4f20cd8591e3e556d3d47282216b
Mehedyh259 <mehedyh259@gmail.com>
2022-06-13 23:26:28 +0600

    secret added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,9 +10,14 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
     // ...add more providers here
   ],
+
+
+
+  secret: process.env.SECRET // SECRET env variable 
+
 })
